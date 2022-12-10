744acca6a16e303340c2829dc3d5f7bfed3850ea
HebaBakry <102521383+HebaBakry@users.noreply.github.com>
2022-12-10 16:08:11 +0200

    color thresholding

--- Rover/perception.py
+++ Rover/perception.py
@@ -3,19 +3,29 @@ import cv2
 
 # Identify pixels above the threshold
 # Threshold of RGB > 160 does a nice job of identifying ground pixels only
-def color_thresh(img, rgb_thresh=(160, 160, 160)):
+def find_navigable(img, rgb_thresh=(150, 150, 150)):
     # Create an array of zeros same xy size as img, but single channel
-    color_select = np.zeros_like(img[:,:,0])
-    # Require that each pixel be above all three threshold values in RGB
-    # above_thresh will now contain a boolean array with "True"
-    # where threshold was met
-    above_thresh = (img[:,:,0] > rgb_thresh[0]) \
-                & (img[:,:,1] > rgb_thresh[1]) \
-                & (img[:,:,2] > rgb_thresh[2])
+    color_select = np.zeros_like(img[:, :, 0])
+    #boolean array
+    above_thresh = (img[:, :, 0] > rgb_thresh[0]) \
+                  & (img[:, :, 1] > rgb_thresh[1]) \
+                  & (img[:, :, 2] > rgb_thresh[2])
     # Index the array of zeros with the boolean array and set to 1
     color_select[above_thresh] = 1
-    # Return the binary image
     return color_select
+
+
+def find_rock(img, rgb_thresh=(110, 110, 50)):
+    #boolean array
+    thresh = (img[:, :, 0] > rgb_thresh[0]) \
+          & (img[:, :, 1] > rgb_thresh[1]) \
+          & (img[:, :, 2] < rgb_thresh[2])
+    # Create an array of zeros same xy size as img, but single channel
+    color_select = np.zeros_like(img[:, :, 0])
+    # Index the array of zeros with the boolean array and set to 1
+    color_select[thresh] = 1
+    return color_select
+
 
 # Define a function to convert from image coords to rover coords
 def rover_coords(binary_img):
