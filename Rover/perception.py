e1757ad1f1093c3b1a111a77fcc8bfe12ff811fc
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2022-12-10 16:47:36 +0200

    Update perception.py

--- Rover/perception.py
+++ Rover/perception.py
@@ -7,9 +7,10 @@ import cv2
 # Define a function to perform a perspective transform
 def perspect_transform(img, src, dst):
     M = cv2.getPerspectiveTransform(src, dst)
+    mask=cv2.warpPerspective(np.ones_like(img[:,:,0]),M, (img.shape[1],img.shape[0]))
     warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
     
-    return warped
+    return warped,mask
 
 dst = 3
 bottom_offset = 5
